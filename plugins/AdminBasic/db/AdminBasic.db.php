5987b62b317aecdb033589b7b76ee137dee0cb67
diegargon <diego@envigo.net>
2018-08-16 17:00:14 +0200

    uninstall

--- plugins/AdminBasic/db/AdminBasic.db.php
+++ plugins/AdminBasic/db/AdminBasic.db.php
@@ -9,3 +9,6 @@ $adminbasic_database = [
     "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('AdminBasic', 'adminbasic_debug', '1');"
 ];
 
+$adminbasic_database_uninstall = [
+    "DELETE FROM `" . DB_PREFIX . "config` WHERE plugin = 'SMBasic'"
+];
