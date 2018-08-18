dcf1678bbca427b9ba96a72b6932ac1453004ba3
diegargon <diego@envigo.net>
2018-08-18 15:03:23 +0200

    add google analytcs

---
+++ plugins/GoogleAnalytics/GoogleAnalytics.php
@@ -0,0 +1,45 @@
+<?php
+/* 
+ *  Copyright @ 2016 - 2018 Diego Garcia
+ */
+!defined('IN_WEB') ? exit : true;
+
+function GoogleAnalytics_init() { 
+    global $tpl;
+   
+    $tpl->addto_tplvar("SCRIPTS_BOTTOM", $tpl->getTPL_file("GoogleAnalytics"));
+}
+
+function GoogleAnalytics_install() {
+    global $db;
+
+    require_once "db/GoogleAnalytics.db.php";
+    foreach ($google_analytics_db_install as $query) {
+        $r = $db->query($query);
+    }
+
+    return ($r) ? true : false;
+}
+
+function GoogleAnalytics_uninstall() {
+    global $db;
+
+    require_once "db/GoogleAnalytics.db.php";
+    foreach ($google_analytics_db_uninstall as $query) {
+        $r = $db->query($query);
+    }
+    return ($r) ? true : false;
+}
+function GoogleAnalytics_preInstall() {
+    return true;
+}
+function GoogleAnalytics_admin_init() {
+    return true;
+}
+function GoogleAnalytics_preInstallInfo() {
+    return true;
+}
+
+function GoogleAnalytics_upgrade($version, $from_version) {
+    return true;
+}
