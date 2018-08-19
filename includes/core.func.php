f92ce9a7e953c0720b5bf78f941476b6cc4208fb
diegargon <diego@envigo.net>
2018-08-19 13:09:19 +0200

    add formatbytes

--- includes/core.func.php
+++ includes/core.func.php
@@ -52,6 +52,14 @@ function mobileDetect() {
     }
 }
 
+function formatBytes($size, $precision = 2)
+{
+    $base = log($size, 1024);
+    $suffixes = array('', 'K', 'M', 'G', 'T');   
+
+    return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+}
+
 /*
 
 
