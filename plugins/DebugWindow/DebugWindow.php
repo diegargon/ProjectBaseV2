921d4f5ed7bf37d298c3a6cc801b1ba5667b3826
diegargon <diego@envigo.net>
2018-09-01 17:14:48 +0200

    debug window

---
+++ plugins/DebugWindow/DebugWindow.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ *  Copyright @ 2016 - 2018 Diego Garcia
+ */
+!defined('IN_WEB') ? exit : true;
+
+function DebugWindow_init() {
+    register_action("add_to_footer", "stats_window");
+    register_action("add_to_footer", "debug_window");
+}
+
+function stats_window() {
+    $content = "<center>";
+    $content .= "<p>Memory usage: " . formatBytes(memory_get_usage()) . " / Mememory peak: ";
+    $content .= formatBytes(memory_get_peak_usage()) . "</p>";
+    $content .= "</center>";
+
+    return $content;
+}
+
+function debug_window() {
+    global $cfg, $db, $debug;
+
+    if (defined('DEBUG')) {
+
+        ($cfg['smbasic_debug']) ? setSessionDebugDetails() : null;
+
+        $q_history = $db->get_query_history();
+        foreach ($q_history as $key => $value) {
+            $debug->log($value, "MYSQL");
+        }
+        $debug_data = "<div style=\"height:250px;width:100%;border:1px solid #ccc;;overflow:auto;\">";
+        $debug_data .= $debug->print_debug();
+        $debug_data .= "</div>";
+        return $debug_data;
+    }
+    return false;
+}
+
+function DebugWindow_install() {
+    return true;
+}
+
+function DebugWindow_preInstall() {
+    return true;
+}
+
+function DebugWindow_preInstall_info() {
+    return true;
+}
+
+function DebugWindow_upgrade($version, $from_version) {
+    return true;
+}
+
+function DebugWindow_uninstall() {
+    return true;
+}
