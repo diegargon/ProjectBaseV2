d3dff92be214712581d394d6f19ae76cb4057d35
diegargon <diego@envigo.net>
2018-09-18 23:37:09 +0200

    remove some spaces from templates safe?

--- plugins/tplBasic/db/tplBasic.db.php
+++ plugins/tplBasic/db/tplBasic.db.php
@@ -18,7 +18,8 @@ $tplbasic_database = [
     "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_css_inline', '1');",
     "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_stats_query', '1')",
     "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_theme', 'default');",
-    "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_gzip', '0');"
+    "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_gzip', '0');",
+    "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('tplBasic', 'tplbasic_remove_spaces', '1');"
 ];
 
 $tplbasic_database_uninstall = [
