5103362950925c85e95e1a3dce24e81ba4700499
diegargon <diego@envigo.net>
2018-09-19 16:33:15 +0200

    remove unused

--- plugins/NewsComments/includes/NewsComments.inc.php
+++ plugins/NewsComments/includes/NewsComments.inc.php
@@ -29,7 +29,6 @@ function News_Comments($news) {
     if (!empty($_POST['btnSendNewComment']) && $cfg['nc_allow_new_comments']) {
         if (!empty($user) || $cfg['nc_allow_anon_comments']) {
             $comm_conf['comment'] = $filter->post_UTF8_txt("news_comment");
-
             $comm_conf['comment'] ? scAddComment($comm_conf) : false;
         }
     }
@@ -50,21 +49,4 @@ function News_Comments($news) {
     $tpl->addto_tplvar("ADD_TO_NEWSSHOW_BOTTOM", $content);
 
     return true;
-}
-
-function News_Comment_Details(& $comment) {
-    global $sm, $cfg, $LNG;
-
-    $author_data = $sm->getUserByID($comment['author_id']);
-    if (!$author_data) {
-        $author_data['uid'] = 0;
-        $author_data['username'] = $LNG['L_SM_DELETED'];
-        $author_data['avatar'] = $cfg['SMB_IMG_DFLT_AVATAR'];
-    }
-    if ($cfg['FRIENDLY_URL']) {
-        $comment['p_url'] = "/{$cfg['WEB_LANG']}/profile&viewprofile={$author_data['uid']}";
-    } else {
-        $comment['p_url'] = "/{$cfg['CON_FILE']}?module=SMBasic&page=profile&viewprofile={$author_data['uid']}&lang={$cfg['WEB_LANG']}";
-    }
-    $comment = array_merge($comment, $author_data);
 }
