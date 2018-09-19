4bb9b80ffc8e397ea2a7ccb1321aed53ac8dd8b8
diegargon <diego@envigo.net>
2018-09-19 16:44:55 +0200

    add option to disable comments in a specific news

--- plugins/NewsComments/db/NewsComments.db.php
+++ plugins/NewsComments/db/NewsComments.db.php
@@ -5,6 +5,10 @@
  */
 !defined('IN_WEB') ? exit : true;
 
+/* */
+$newsComments_database_install[] = "
+    ALTER TABLE `" . DB_PREFIX . "news` ADD `comments_disabled` TINYINT(1) NOT NULL DEFAULT '1' AFTER `tags`;
+";
 /* Config */
 $newsComments_database_install[] = "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('NewsComments', 'newscomments_debug', '0');";
 $newsComments_database_install[] = "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('NewsComments', 'nc_max_comments_perpage', '10');";
@@ -12,6 +16,7 @@ $newsComments_database_install[] = "INSERT INTO `" . DB_PREFIX . "config` (`plug
 $newsComments_database_install[] = "INSERT INTO `" . DB_PREFIX . "config` (`plugin`, `cfg_key`, `cfg_value`) VALUES ('NewsComments', 'nc_allow_new_comments', '1');";
 /* UNINSTALL */
 $newsComments_database_uninstall = [
+    "ALTER TABLE `" . DB_PREFIX . "news` DROP `comments_disabled`;",
     "DELETE FROM `" . DB_PREFIX . "plugins` WHERE plugin_name = 'NewsComments'",
     "DELETE FROM `" . DB_PREFIX . "config` WHERE plugin = 'NewsComments'"
 ];
