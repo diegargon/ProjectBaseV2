6425b855d68387d87bc4faaac0c7c17092fe497c
diegargon <diego@envigo.net>
2018-09-19 13:13:47 +0200

    sm minor fix

--- plugins/SMBasic/tpl/viewprofile.tpl.php
+++ plugins/SMBasic/tpl/viewprofile.tpl.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright @ 2016 Diego Garcia
+ *  Copyright @ 2016 - 2018 Diego Garcia
  */
 !defined('IN_WEB') ? exit : true;
 ?>
@@ -25,11 +25,11 @@
             </dl>
             <dl>
                 <dt><label><?= $LNG['L_SM_REGISTERED'] ?></label></dt>
-                <dd><span><?= format_date($data['regdate']) ?> </span></dd>
+                <dd><span><?= $tUtil->format_date($data['regdate']) ?> </span></dd>
             </dl>
             <dl>
                 <dt><label><?= $LNG['L_SM_LASTLOGIN'] ?> </label></dt>
-                <dd><span><?= format_date($data['last_login']) ?> </span></dd>
+                <dd><span><?= $tUtil->format_date($data['last_login']) ?> </span></dd>
             </dl>
             <?= !empty($tpldata['SMB_VIEWPROFILE_FIELDS_BOTTOM']) ? $tpldata['SMB_VIEWPROFILE_FIELDS_BOTTOM'] : false; ?>            
         </div>
