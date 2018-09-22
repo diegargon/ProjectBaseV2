5c2fdd03623e0e858491ccf9b41ae1a51a156ab9
diegargon <diego@envigo.net>
2018-09-22 15:02:17 +0200

    rid "

--- plugins/News/tpl/news_section_article.tpl.php
+++ plugins/News/tpl/news_section_article.tpl.php
@@ -5,7 +5,7 @@
 !defined('IN_WEB') ? exit : true;
 ?>
 <a href="<?= $data['url'] ?>">
-    <article class="newsbox <?= $data['featured'] ? "featured" : null ?>">
+    <article class="newsbox <?= $data['featured'] ? 'featured' : null ?>">
         <p class='p-small'><?= $data['date'] ?></p>
         <h3><?= $data['title'] ?></h3>
         <p><?= $data['lead'] ?></p>
