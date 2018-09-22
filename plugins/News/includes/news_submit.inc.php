09ddec63ea09d36a757031e5d62eeb402e774528
diegargon <diego@envigo.net>
2018-09-22 16:42:31 +0200

    fix news lang problem and change "->'

--- plugins/News/includes/news_submit.inc.php
+++ plugins/News/includes/news_submit.inc.php
@@ -9,7 +9,7 @@
 function news_new_form($news_perms) {
     global $LNG, $cfg, $acl_auth, $tpl, $sm, $frontend, $ml, $plugins;
 
-    if (!($plugins->express_start_provider("EDITOR")) || !($plugins->express_start_provider("CATS"))) {
+    if (!($plugins->express_start_provider('EDITOR')) || !($plugins->express_start_provider('CATS'))) {
         $frontend->message_box(['msg' => 'L_E_PL_CANTEXPRESS']);
         return false;
     }
@@ -38,7 +38,7 @@ function news_new_form($news_perms) {
     $form_data['news_add_source'] = $news_perms['news_add_source'];
     $form_data['news_add_related'] = $news_perms['news_add_related'];
 
-    if (defined('MULTILANG') && ($site_langs = $ml->get_sitelangs_select("news_lang")) != false) {
+    if (defined('MULTILANG') && ($site_langs = $ml->get_sitelangs_select('news_lang')) != false) {
         $form_data['select_langs'] = $site_langs;
     }
 
@@ -47,22 +47,22 @@ function news_new_form($news_perms) {
         $frontend->message_box(['msg' => 'L_NEWS_NOCATS']);
         return false;
     }
-    $form_data['terms_url'] = ""; // $cfg['TERMS_URL'];    
-    do_action("news_new_form_add", $form_data);
+    $form_data['terms_url'] = ''; // $cfg['TERMS_URL'];    
+    do_action('news_new_form_add', $form_data);
 
     /* EDITOR */
 
     $form_data['editor'] = $editor->getEditor();
 
-    $tpl->addto_tplvar("ADD_TO_BODY", $tpl->getTPL_file("News", "news_form", $form_data));
+    $tpl->addto_tplvar('ADD_TO_BODY', $tpl->getTPL_file('News', 'news_form', $form_data));
 }
 
 function news_create_new($news_data) {
     global $cfg, $ml, $db;
 
-    $news_data['nid'] = $db->get_next_num("news", "nid");
+    $news_data['nid'] = $db->get_next_num('news', 'nid');
 
-    defined('MULTILANG') ? $lang_id = $ml->get_web_lang_id() : $lang_id = 1;
+    defined('MULTILANG') ? $lang_id = $ml->iso_to_id($news_data['news_lang']) : $lang_id = 1;
 
     empty($news_data['featured']) ? $news_data['featured'] = 0 : null;
 
@@ -75,46 +75,43 @@ function news_create_new($news_data) {
     }
 
     $insert_ary = [
-        "nid" => $news_data['nid'],
-        "lang_id" => $lang_id,
-        "page" => 1,
-        "title" => $db->escape_strip($news_data['title']),
-        "lead" => $db->escape_strip($news_data['lead']),
-        "text" => $db->escape_strip($news_data['editor_text']),
-        "featured" => $news_data['featured'],
-        "author_id" => $news_data['author_id'],
-        "category" => $news_data['category'],
-        "lang" => $news_data['news_lang'],
-        "moderation" => $moderation
+        'nid' => $news_data['nid'],
+        'lang_id' => $lang_id,
+        'page' => 1,
+        'title' => $db->escape_strip($news_data['title']),
+        'lead' => $db->escape_strip($news_data['lead']),
+        'text' => $db->escape_strip($news_data['editor_text']),
+        'featured' => $news_data['featured'],
+        'author_id' => $news_data['author_id'],
+        'category' => $news_data['category'],
+        'moderation' => $moderation
     ];
 
-    do_action("news_submit_insert", $insert_ary);
-    $db->insert("news", $insert_ary);
+    do_action('news_submit_insert', $insert_ary);
+    $db->insert('news', $insert_ary);
 
     /* Custom / MOD */
-    do_action("news_create_new_insert", $news_data);
+    do_action('news_create_new_insert', $news_data);
 
-    $plugin = "News";
+    $plugin = 'News';
 
     //SOURCE LINK
     if (!empty($news_data['news_source'])) {
-        $type = "source";
         $insert_ary = [
-            "source_id" => $news_data['nid'],
-            "plugin" => $plugin,
-            "type" => $type,
-            "link" => $db->escape_strip(urlencode($news_data['news_source']))
+            'source_id' => $news_data['nid'],
+            'plugin' => $plugin,
+            'type' => 'source',
+            'link' => $db->escape_strip(urlencode($news_data['news_source']))
         ];
-        $db->insert("links", $insert_ary);
+        $db->insert('links', $insert_ary);
     }
     //NEW RELATED
     if (!empty($news_data['news_new_related'])) {
-        $type = "related";
         $insert_ary = [
-            "source_id" => $news_data['nid'], "plugin" => $plugin,
-            "type" => $type, "link" => $db->escape_strip(urlencode($news_data['news_new_related'])),
+            'source_id' => $news_data['nid'], 'plugin' => $plugin,
+            'type' => 'related', 'link' => $db->escape_strip(urlencode($news_data['news_new_related'])),
         ];
-        $db->insert("links", $insert_ary);
+        $db->insert('links', $insert_ary);
     }
     return true;
 }
