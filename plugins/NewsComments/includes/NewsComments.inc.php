79b280c57e768b299b84430cd8b04534766119bd
diegargon <diego@envigo.net>
2018-09-28 19:52:29 +0200

    fix var name

--- plugins/NewsComments/includes/NewsComments.inc.php
+++ plugins/NewsComments/includes/NewsComments.inc.php
@@ -78,7 +78,7 @@ function NewsComments_AddrateDisplay(& $comments) {
     $rating_r_ids = '';
 
     foreach ($comments as $comment) {
-        empty($rating_r_cids) ? $rating_r_ids = $comment['cid'] : $rating_r_cids .= ',' . $comment['cid'];
+        empty($rating_r_ids) ? $rating_r_ids = $comment['cid'] : $rating_r_ids .= ',' . $comment['cid'];
     }
 
     $ratings_data = ratings_get_ratings($rating_r_ids, 'news_comments_rate');
