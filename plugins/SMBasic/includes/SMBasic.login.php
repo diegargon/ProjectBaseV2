301e00f0312d88ef3f3629b0b6646c56d93a25e0
diegargon <diego@envigo.net>
2019-05-19 21:13:28 +0200

    activation fix & '" work

--- plugins/SMBasic/includes/SMBasic.login.php
+++ plugins/SMBasic/includes/SMBasic.login.php
@@ -14,7 +14,7 @@ function SMBasic_Login($email, $password, $rememberme) {
         die('[{"status": "error", "msg": "' . $LNG['L_E_INTERNAL'] . '"}]');
     }
 
-    $query = $db->select_all("users", ["email" => "$email", "password" => "$password_encrypt"], "LIMIT 1");
+    $query = $db->select_all('users', ['email' => $email, 'password' => $password_encrypt], 'LIMIT 1');
     if (($user = $db->fetch($query))) {
         $db->free($query);
 
@@ -43,12 +43,12 @@ function SMBasic_user_activate_account($activation_code) {
 
     $activation_code = $db->escape_strip($activation_code);
 
-    $query = $db->select_all("users", ["active" => $activation_code], "LIMIT 1");
+    $query = $db->select_all('users', ['active' => $activation_code], 'LIMIT 1');
     if ($db->num_rows($query) <= 0) {
         return false;
     }
 
-    $db->update("users", ["active" => 1], ["active" => $activation_code]);
+    $db->update('users', ['active' => 1], ['active' => $activation_code]);
 
     return true;
 }
@@ -56,7 +56,7 @@ function SMBasic_user_activate_account($activation_code) {
 function SMBasic_RequestResetOrActivation() {
     global $LNG, $cfg, $db, $filter;
 
-    if (($email = $filter->post_email("email")) == false) {
+    if (($email = $filter->post_email('email')) == false) {
         die('[{"status": "1", "msg": "' . $LNG['L_E_EMAIL'] . '"}]');
         return false;
     }
@@ -64,7 +64,7 @@ function SMBasic_RequestResetOrActivation() {
         die('[{"status": "1", "msg": "' . $LNG['L_EMAIL_LONG'] . '"}]');
         return false;
     }
-    $query = $db->select_all("users", ["email" => "$email"], "LIMIT 1");
+    $query = $db->select_all('users', ['email' => $email], 'LIMIT 1');
     if ($db->num_rows($query) <= 0) {
         die('[{"status": "1", "msg": "' . $LNG['L_E_EMAIL_NOEXISTS'] . '"}]');
     } else {
@@ -75,9 +75,9 @@ function SMBasic_RequestResetOrActivation() {
             die('[{"status": "2", "msg": "' . $LNG['L_ACTIVATION_EMAIL'] . '"}]');
         } else {
             $reset = mt_rand(11111111, 2147483647);
-            $db->update("users", ["reset" => "$reset"], ["email" => $db->escape_strip($email)]);
-            $URL = $cfg['WEB_URL'] . $cfg['WEB_LANG'] . "/login" . "&reset=$reset&email=$email";
-            $msg = $LNG['L_RESET_EMAIL_MSG'] . "\n" . "$URL";
+            $db->update('users', ['reset' => $reset], ['email' => $db->escape_strip($email)]);
+            $URL = $cfg['WEB_URL'] . $cfg['WEB_LANG'] . '/login' . "&reset=$reset&email=$email";
+            $msg = $LNG['L_RESET_EMAIL_MSG'] . '\n' . $URL;
             mail($email, $LNG['L_RESET_EMAIL_SUBJECT'], $msg, "From: {$cfg['smbasic_register_reply_email']} \r\n");
             die('[{"status": "2", "msg": "' . $LNG['L_RESET_EMAIL'] . '"}]');
         }
@@ -94,14 +94,14 @@ function SMBasic_user_reset_password() {
     if ($reset == false || $email == false) {
         return false;
     }
-    $query = $db->select_all("users", ["email" => "$email", "reset" => "$reset"]);
+    $query = $db->select_all('users', ['email' => $email, 'reset' => $reset]);
     if ($db->num_rows($query) > 0) {
         $user = $db->fetch($query);
         $password = SMBasic_randomPassword();
         $password_encrypted = $sm->encryptPassword($password);
-        $db->update("users", ["password" => "$password_encrypted", "reset" => "0"], ["uid" => "{$user['uid']}"]);
-        $URL = "{$cfg['WEB_URL']}" . "login";
-        $msg = $LNG['L_RESET_SEND_NEWMAIL_MSG'] . "\n" . "$password\n" . "$URL";
+        $db->update('users', ['password' => $password_encrypted, 'reset' => '0'], ['uid' => $user['uid']]);
+        $URL = "{$cfg['WEB_URL']}" . 'login';
+        $msg = $LNG['L_RESET_SEND_NEWMAIL_MSG'] . '\n' . "$password\n" . $URL;
         mail($email, $LNG['L_RESET_SEND_NEWMAIL_SUBJECT'], $msg, "From: {$cfg['smbasic_register_reply_email']} \r\n");
         $frontend->messageBox(['msg' => 'L_RESET_PASSWORD_SUCCESS']);
         return true;
@@ -125,6 +125,6 @@ function SMBasic_randomPassword() {
 function SMBasic_LoginScripts() {
     global $tpl;
 
-    $tpl->addScriptFile("standard", "jquery", "TOP");
-    $tpl->addScriptFile("SMBasic", "login", "BOTTOM");
+    $tpl->addScriptFile('standard', 'jquery', 'TOP');
+    $tpl->addScriptFile('SMBasic', 'login', 'BOTTOM');
 }
--- plugins/SMBasic/login.php
+++ plugins/SMBasic/login.php
@@ -5,16 +5,16 @@
  */
 !defined('IN_WEB') ? exit : true;
 
-if (!($sm->getPerm("login_enable"))) {
-    $frontend->messageBox(['msg' => "L_E_LOGGIN_DISABLE"]);
+if (!($sm->getPerm('login_enable'))) {
+    $frontend->messageBox(['msg' => 'L_E_LOGGIN_DISABLE']);
     return;
 }
 
 $user = $sm->getSessionUser();
 
 //HEAD MOD
-$cfg['PAGE_TITLE'] = $cfg['WEB_NAME'] . ": " . $LNG['L_LOGIN'];
-$cfg['PAGE_DESC'] = $cfg['WEB_NAME'] . ": " . $LNG['L_LOGIN'];
+$cfg['PAGE_TITLE'] = $cfg['WEB_NAME'] . ': ' . $LNG['L_LOGIN'];
+$cfg['PAGE_DESC'] = $cfg['WEB_NAME'] . ': ' . $LNG['L_LOGIN'];
 //END HEAD MOD
 
 if ($user && $user['uid'] > 0) {
@@ -22,18 +22,18 @@ if ($user && $user['uid'] > 0) {
     return;
 }
 
-require_once("includes/SMBasic.login.php");
+require_once('includes/SMBasic.login.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['active'])) {
-        if (($activation_code = $filter->get_int('active', 1, 1)) &&
+        if (($activation_code = $filter->get_int('active', 10, 1)) &&
                 !SMBasic_user_activate_account($activation_code)
         ) {
-            $msgbox['title'] = "L_SM_REGISTERED";
-            $msgbox['msg'] = "L_SM_E_ACTIVATION";
+            $msgbox['title'] = 'L_SM_REGISTERED';
+            $msgbox['msg'] = 'L_SM_E_ACTIVATION';
         } else {
-            $msgbox['title'] = "L_SM_TITLE_OK";
-            $msgbox['msg'] = "L_SM_ACTIVATION_OK";
+            $msgbox['title'] = 'L_SM_TITLE_OK';
+            $msgbox['msg'] = 'L_SM_ACTIVATION_OK';
         }
         $msgbox['backlink'] = $cfg['WEB_URL'];
         $frontend->messageBox($msgbox);
@@ -42,10 +42,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     if (isset($_GET['reset'])) {
         if (!SMBasic_user_reset_password()) {
-            $msgbox['msg'] = "L_SM_E_RESET";
+            $msgbox['msg'] = 'L_SM_E_RESET';
         } else {
             $msgbox['title'] = 'L_SM_TITLE_OK';
-            $msgbox['msg'] = "L_SM_RESET_OK";
+            $msgbox['msg'] = 'L_SM_RESET_OK';
         }
         $msgbox['backlink'] = $cfg['WEB_URL'];
         $frontend->messageBox($msgbox);
@@ -55,9 +55,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['email']) && !empty($_POST['password'])) {
-        $email = $filter->post_email("email");
-        $password = $filter->post_password("password");
-        ($filter->post_int("rememberme")) ? $rememberme = 1 : $rememberme = 0;
+        $email = $filter->post_email('email');
+        $password = $filter->post_password('password');
+        ($filter->post_int('rememberme')) ? $rememberme = 1 : $rememberme = 0;
         if ($email != false && $password != false) {
             SMBasic_Login($email, $password, $rememberme);
         } else {
@@ -77,13 +77,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       }
       }
      */
-    $tpl->getCssFile("SMBasic");
-    $tpl->getCssFile("SMBasic", "SMBasic-mobile");
+    $tpl->getCssFile('SMBasic');
+    $tpl->getCssFile('SMBasic', 'SMBasic-mobile');
     SMBasic_LoginScripts();
     if ($cfg['FRIENDLY_URL']) {
         $login_data['register_url'] = "/{$cfg['WEB_LANG']}/register";
     } else {
         $login_data['register_url'] = "/{$cfg['CON_FILE']}?module=SMBasic&page=register&lang={$cfg['WEB_LANG']}";
     }
-    $tpl->addtoTplVar("ADD_TO_BODY", $tpl->getTplFile("SMBasic", "login", $login_data));
+    $tpl->addtoTplVar('ADD_TO_BODY', $tpl->getTplFile('SMBasic', 'login', $login_data));
 }
